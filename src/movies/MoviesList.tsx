cc2126e28c1c58b1324cb65821e39c1ff9da95e7
Метелица Богдан <bogdan.metelitsa@gmail.com>
2023-02-17 01:54:03 +0300

    Adding a bootstrap library, creating a button component

--- src/movies/MoviesList.tsx
+++ src/movies/MoviesList.tsx
@@ -1,3 +1,4 @@
+import GenericList from "../utils/GenericList";
 import Loading from "../utils/Loading";
 import IndividualMovie from "./IndividualMovie";
 import { movieDTO } from "./Movies.model";
@@ -5,22 +6,13 @@ import css from './MoviesList.module.css'
 
 export default function MoviesList(props: moviesListProps)
 {
-    if(!props.movies)
-    {
-        return <><Loading></Loading></>
-    } else if(props.movies.length === 0)
-    {
-        return <>There are no movies to display.</>
-    }
-    else
-    {
-        return(
+    return (<GenericList list={props.movies}>
             <div className={css.div}>
-                {props.movies.map(movie =>
+                {props.movies?.map(movie =>
                     <IndividualMovie{...movie} key={movie.id} />)}
             </div>
-        )
-    }
+    </GenericList>);
+            
 }
 
 interface moviesListProps{
