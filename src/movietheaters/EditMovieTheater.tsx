c2510dc5eb3584613d70bf4a62cc1b03147d8471
Метелица Богдан <bogdan.metelitsa@gmail.com>
2023-02-27 00:15:12 +0300

    Creating Map component and MapField

--- src/movietheaters/EditMovieTheater.tsx
+++ src/movietheaters/EditMovieTheater.tsx
@@ -6,7 +6,8 @@ export default function EditMovieTheater()
         <>
             <h3>Edit Movie Theater</h3>
             <MovieTheaterForm 
-            model={{name: "Some movie theater"}}
+            model={{name: "Some movie theater", latitude:59.93358523219539,
+             longitude: 30.34255385398865}}
             onSubmit={values => console.log(values)}
             />
         </>
