fa5137c0d96cd8a6ebd44a92d1c344161300a925
Метелица Богдан <bogdan.metelitsa@gmail.com>
2023-02-26 23:14:10 +0300

    Creating MovieTheaterForm

--- src/movietheaters/EditMovieTheater.tsx
+++ src/movietheaters/EditMovieTheater.tsx
@@ -1,9 +1,14 @@
+import MovieTheaterForm from "./MovieTheaterForm";
+
 export default function EditMovieTheater()
 {
     return(
         <>
             <h3>Edit Movie Theater</h3>
-            
+            <MovieTheaterForm 
+            model={{name: "Some movie theater"}}
+            onSubmit={values => console.log(values)}
+            />
         </>
     )
 }
