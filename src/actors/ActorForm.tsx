f265f3ed856a0201ccae266d2922413c0b0cadc7
Метелица Богдан <bogdan.metelitsa@gmail.com>
2023-02-26 22:41:56 +0300

    Creating MarkdownField

--- src/actors/ActorForm.tsx
+++ src/actors/ActorForm.tsx
@@ -6,6 +6,7 @@ import actorCreationDTO from "./actors.model";
 import * as Yup from 'yup'
 import DateField from "../forms/DateField";
 import ImageField from "../forms/ImageField";
+import MarkdownField from "../forms/MarkdownField";
 
 export default function ActorForm(props: actorFormProps)
 {
@@ -23,6 +24,7 @@ export default function ActorForm(props: actorFormProps)
                     <TextField field="name" displayName="Name" />
                     <DateField displayName="Date of Birth" field="dateOfBirth"/>
                     <ImageField displayName="Picture" field="picture" imageURL={props.model.pictureURL}></ImageField>
+                    <MarkdownField displayName="Biography" field="biography"/>
                     <Button disabled={formikProps.isSubmitting} type="submit">Save Changes</Button>
                     <Link to="/actors" className="btn btn-secondary">Cancel</Link>
                 </Form>
