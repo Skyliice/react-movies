d296d08d0bce1e6f36a6fcfb6d47d43a972a5759
Метелица Богдан <bogdan.metelitsa@gmail.com>
2023-03-02 02:20:23 +0300

    Creating httpRequests to back end

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -4,18 +4,27 @@ import Button from "../utils/Button";
 import * as Yup from 'yup'
 import TextField from "../forms/TextField";
 import GenreForm from "./GenreForm";
+import { genreCreationDTO } from "./genres.model";
+import axios from "axios";
+import { urlGenres } from "../endpoints";
 
 export default function CreateGenre() {
     const history = useHistory();
+    async function create(genre: genreCreationDTO)
+    {
+        try{
+            await axios.post(urlGenres,genre);
+            history.push("/genres");
+        }
+        catch(error)
+        {
+            console.error(error);
+        }
+    }
     return (
         <>
             <h3>Create Genre</h3>
-            <GenreForm model={{name: ""}} onSubmit={
-                async value => {
-                    // when the form is posted
-                    console.log(value);
-                }
-            }/>
+            <GenreForm model={{name: ""}} onSubmit={ async value => await create(value)}/>
         </>
     )
 }
