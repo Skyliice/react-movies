34b14a34326d989e222f779afaf171b9aef1b74a
Метелица Богдан <bogdan.metelitsa@gmail.com>
2023-03-04 23:41:56 +0300

    Creating DisplayErrors component

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -7,23 +7,30 @@ import GenreForm from "./GenreForm";
 import { genreCreationDTO } from "./genres.model";
 import axios from "axios";
 import { urlGenres } from "../endpoints";
+import { useState } from "react";
+import DisplayErrors from "../utils/DisplayErrors";
 
 export default function CreateGenre() {
     const history = useHistory();
+    const [errors, setError] = useState<string[]>([]);
     async function create(genre: genreCreationDTO)
     {
         try{
             await axios.post(urlGenres,genre);
             history.push("/genres");
         }
-        catch(error)
+        catch(error: any)
         {
-            console.error(error);
+            if(error && error.response)
+            {
+                setError(error.response.data);
+            }
         }
     }
     return (
         <>
             <h3>Create Genre</h3>
+            <DisplayErrors errors={errors}/>
             <GenreForm model={{name: ""}} onSubmit={ async value => await create(value)}/>
         </>
     )
--- src/genres/GenreForm.tsx
+++ src/genres/GenreForm.tsx
@@ -12,7 +12,7 @@ export default function GenreForm(props: genreFormProps)
         <Formik initialValues={props.model} onSubmit={props.onSubmit} validationSchema={Yup.object({
                 name: Yup.string()
                 .required("This field is required")
-                .max(30,"Max length is 30 characters").firstLetterUppercase()
+                .max(30,"Max length is 30 characters")
             })}>
                 {(formikProps) => (
                     <Form>
