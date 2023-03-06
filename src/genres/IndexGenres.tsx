0492ea806b64255ffd55171d30d341a364d34826
Метелица Богдан <bogdan.metelitsa@gmail.com>
2023-03-06 23:39:58 +0300

    Creating pagination

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -29,7 +29,7 @@ export default function IndexGenres()
                     {genres?.map(genre =>
                         <tr key={genre.id}>
                             <td>
-                                <Link className="btn btn success" 
+                                <Link className="btn btn-success" 
                                 to={`/genres/${genre.id}`}>Edit</Link>
                                 <Button className="btn btn-danger">Delete</Button>
                             </td>
